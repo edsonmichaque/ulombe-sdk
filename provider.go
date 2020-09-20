80ce5c6399ac55dc80335c74af5dd1edaa81d648
Edson Michaque <edson@michaque.com>
2020-09-20 10:34:56 +0200

    Add AddScriptGeneratorFunction

--- provider.go
+++ provider.go
@@ -26,3 +26,23 @@ func (p Provider) AddOperation(o provider.Operation) {
 func (p Provider) GenerateScript(op string, data map[string]interface{}) string {
 	return  p.ScriptGenerator(op, data)
 }
+
+func (p Provider) AddScriptGenerator(function func(pro *Provider) func(op string, data map[string]interface{}) string) {
+	p.ScriptGenerator = function(&p)
+}
+
+func (p Provider) GetOperation(operation string) string {
+	for k, v := range(p.Operations) {
+		if k == operation {
+			return k
+		}
+
+		for k2, v2 := range v.Aliases {
+			if v2 == operation {
+				return k
+			}
+		}
+	}
+
+	return nil
+}
